fd1f7f6583ce75da1ab4bf5a0201e187a8b8986b
Angelo 'Flecart' Huang <huangelo02@gmail.com>
2022-07-13 13:01:23 +0200

    fix(server): bind and close socket bug
    couldnt bind again after closing the socket, need to create everytime a new socket, + refactor with cubbit style

--- level3-io/src/Server.ts
+++ level3-io/src/Server.ts
@@ -5,59 +5,30 @@ export default class Server {
     private _socket: dgram.Socket; 
     private _clients: {[id: number] : IClient}; 
 
-    constructor() {
-        this._socket = dgram.createSocket("udp4");
-    }
+    private _createSocket(): dgram.Socket 
+    {
+        let socket: dgram.Socket = dgram.createSocket('udp4');
 
-    private _registerClient(message: IMessage): void {
-        const client: IClient = JSON.parse(message.payload);
-        this._clients[client.id] = client;
-    }
-
-    private _unregisterClient(message: IMessage): void {
-        delete this._clients[message.source.id];
-    }
-
-    private _sendMessage(message: IMessage): void {
-        const client: IClient = this._clients[message.destination];
-        const payload = JSON.stringify(message);
-        this._socket.send(payload, client.address.port, client.address.ip, (err, bytes) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
-
-    private _broadcastMessage(message: IMessage): void {
-        for (const id of Object.keys(this._clients)) {
-            if (Number(id) === message.source.id) { // don't send to source
-                continue; 
-            }
-
-            const client = this._clients[id];
-            this._socket.send(message.payload, client.address.port, client.address.ip);
-        }
-    }
-
-    listen(port?: number | ((port: number) => void), callback?: (port: number) => void) {
-        const defaultPort = 8000;
-        if (port === undefined && callback === undefined) {
-            port = defaultPort;
-        } else if (callback === undefined) { // only port (callback) defined
-            callback = port as ((port: number) => void);
-            port = defaultPort;
-        } // else they are both defined
-        this._port = port as number;
-
-        this._socket.on('error', (err) => {
+        socket.on('error', (err) => 
+        {
             console.log(`server error:\n${err.stack}`);
-            this._socket.close();
+            socket.close();
         });
+
+        socket.on('listening', () => {
+            let address = socket.address();
+            console.log(`server listening ${address.address}:${address.port}`);
+        })
+
+        socket.on('close', () => {
+            console.log('server closed');
+        })
           
-        this._socket.on('message', (msg, rinfo) => {
+        socket.on('message', (msg, rinfo) => {
             // TODO should validate messages are all well formed
             const message: IMessage = JSON.parse(msg.toString());
-            switch (message.type) {
+            switch (message.type) 
+            {
                 case MessageType.REGISTRATION:
                     this._registerClient(message);
                     break; 
@@ -75,24 +46,70 @@ export default class Server {
             }
             console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
         });
-        
-        this._socket.on('listening', () => {
-            const address = this._socket.address();
-            console.log(`server listening ${address.address}:${address.port}`);
+
+        return socket;
+    };
+
+
+    private _registerClient(message: IMessage): void 
+    {
+        const client: IClient = JSON.parse(message.payload);
+        this._clients[client.id] = client;
+    }
+
+    private _unregisterClient(message: IMessage): void 
+    {
+        delete this._clients[message.source.id];
+    }
+
+    private _sendMessage(message: IMessage): void 
+    {
+        const client: IClient = this._clients[message.destination];
+        const payload = JSON.stringify(message);
+        this._socket.send(payload, client.address.port, client.address.ip, (err, bytes) => 
+        {
+            if (err) 
+                console.log(err);
         });
+    }
 
-        this._socket.on("close", () => {
-            console.log("server closed");
-        });
+    private _broadcastMessage(message: IMessage): void 
+    {
+        for(const id of Object.keys(this._clients)) 
+        {
+            if(Number(id) === message.source.id)// don't send to source
+                continue; 
 
-        this._socket.bind(this._port);
-
-        if (callback !== undefined) {
-            callback(this._port);
+            const client = this._clients[id];
+            this._socket.send(message.payload, client.address.port, client.address.ip);
         }
     }
 
-    shutdown(callback?: () => void) {
-        this._socket.close(callback);
+    public listen(port?: number | ((port: number) => void), callback?: (port: number) => void) 
+    {
+        const defaultPort = 8000;
+        if(port === undefined && callback === undefined) 
+            port = defaultPort;
+        else if(callback === undefined) // only port (callback) defined 
+        { 
+            callback = port as ((port: number) => void);
+            port = defaultPort;
+        } // else they are both defined
+        this._port = port as number;
+        this._socket = this._createSocket();
+        
+        this._socket.bind(this._port, () => 
+        {
+            if(callback !== undefined)
+                callback(this._port);
+        });
+
+    }
+
+    public shutdown(callback?: () => void) 
+    {
+        this._socket.close(); // close can't have new connections
+        if (callback !== undefined) 
+            callback();
     }
 }
--- level3-io/src/index.ts
+++ level3-io/src/index.ts
@@ -2,6 +2,3 @@ import Client from './Client';
 import Server from './Server';
 
 export {Client, Server};
-
-const server = new Server(); 
-server.listen();
